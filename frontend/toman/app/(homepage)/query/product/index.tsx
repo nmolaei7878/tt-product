b741be4372f8029bf8f86aa7183df2118c14f0ea
mohammad molaei <mohammadmolaei7800@gmail.com>
2025-12-05 16:51:31 +0330

    change api to recives filters

--- frontend/toman/app/(homepage)/query/product/index.tsx
+++ frontend/toman/app/(homepage)/query/product/index.tsx
@@ -1,41 +1,27 @@
 import axiosClient from '@/_shared/config/axios';
 import { toErrorResponse } from '@/_shared/config/error';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import {
-  mapProductResponseDto,
-  ProductPayload,
-  ProductResponseDto,
-} from './types';
+import { ProductPayload, ProductResponseDto } from './types';
 
-export async function fetchProducts(
+const fetchProducts = async (
   payload: ProductPayload
-): Promise<ProductResponseDto> {
+): Promise<ProductResponseDto> => {
   try {
-    const res = await axiosClient.get('/api/products', {
-      params: { ...payload },
-    });
-
-    return mapProductResponseDto(res.data);
+    const res = await axiosClient.get('/api/products', { params: payload });
+    return res.data;
   } catch (e) {
     throw toErrorResponse(e);
   }
-}
+};
 
-export function useFetchProductsInfinite(
-  payload: Omit<ProductPayload, 'page'>
-) {
+export const useFetchProductsInfinite = (
+  filters: Omit<ProductPayload, 'page'>
+) => {
   return useInfiniteQuery({
-    queryKey: ['products', payload],
+    queryKey: ['products', filters],
     initialPageParam: 1,
-
-    queryFn: async ({ pageParam }) => {
-      return fetchProducts({
-        ...payload,
-        page: pageParam,
-      });
-    },
-
-    getNextPageParam: (lastPage) =>
-      lastPage.hasMore ? lastPage.page + 1 : undefined,
+    queryFn: ({ pageParam = 1 }) =>
+      fetchProducts({ ...filters, page: pageParam }),
+    getNextPageParam: (last) => (last.hasMore ? last.page + 1 : undefined),
   });
-}
+};
